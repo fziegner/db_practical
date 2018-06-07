534a950ff01ce685a7a7a348d25ccfebb25dadbd
Steven Lehmann <sl74byra@studserv.uni.leipzig.de>
2018-06-07 11:50:43 +0200

    Vollgas Aufgabe 2

--- Aufgabe 2/queries.sql
+++ Aufgabe 2/queries.sql
@@ -1,1 +1,6 @@
 -- Query 1
+SELECT count (distinct city.cityid)
+FROM city JOIN country ON city.country = country.countryid 
+   JOIN continent ON country.continent = continent.continentid 
+   JOIN University ON city.cityid = city
+WHERE continent.continentname = 'Africa';
