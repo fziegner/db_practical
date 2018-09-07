3cf6388b23bee72cf19ad178be3cab41c7a6f8f2
Fabian Ziegner <fz31fovo@studserv.uni-leipzig.de>
2018-09-07 18:55:43 +0200

    Added Hibernate config file

--- Aufgabe 3/src/main/Hibernate.java
+++ Aufgabe 3/src/main/Hibernate.java
@@ -1,13 +1,15 @@
 package main;
 
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Hibernate {
 	
 	private static SessionFactory sessionFactory;
 	
+	/*
 	static {
 		try {
 			sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -17,6 +19,22 @@ public class Hibernate {
 			System.err.println("Failed Initializing Hibernate");
 		}
 	}
+	*/
+	
+	public void setup() {
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+		        .configure() // configures settings from hibernate.cfg.xml
+		        .build();
+		try {
+		    sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		} catch (Exception ex) {
+		    StandardServiceRegistryBuilder.destroy(registry);
+		}
+    }
+ 
+	public void exit() {
+		sessionFactory.close();
+    }
 	
 	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
--- Aufgabe 3/src/main/Main.java
+++ Aufgabe 3/src/main/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Hallo");
+		
+		Hibernate hibernate = new Hibernate();
+		
+		hibernate.setup();
+		hibernate.exit();
 	}
-
 }
