08a1fc7cab9136666913abbd01c93560cf78c70a
Steven Lehmann <sl74byra@studserv.uni-leipzig.de>
2018-09-08 14:32:02 +0200

    Comments & Post Class

--- Aufgabe 3 Maven/src/main/java/model/Comments.java
+++ Aufgabe 3 Maven/src/main/java/model/Comments.java
@@ -1,5 +1,137 @@
 package model;
 
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 public class Comments implements Message {
 
+	@Id
+	@Column(name = "commentID")
+	private Long commentID;
+	
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar creationDate;
+	
+	@JoinColumn(name = "person")
+	private Long creatorID;
+	
+	@Column(length = 64)
+	private String browserUsed;
+	
+	@Column(nullable = false)
+	private String locationIP;
+	
+	@Column(columnDefinition = "text")
+	private String text;
+	
+	private int length;
+	
+	@JoinColumn(name = "comment")
+	private Long replyOfComment;
+	
+	@JoinColumn(name = "post")
+	private Long replyOfPost;
+	
+	@Column(nullable = false)
+	@JoinColumn(name = "country")
+	private int location;
+
+	public Comments() {
+	}
+
+	public Comments(Long commentID, Calendar creationDate, Long creatorID, String browserUsed, String locationIP, String text, int length, Long replyOfComment, Long replyOfPost, int location) {
+		this.commentID = commentID;
+		this.creationDate = creationDate;
+		this.creatorID = creatorID;
+		this.browserUsed = browserUsed;
+		this.locationIP = locationIP;
+		this.text = text;
+		this.length = length;
+		this.replyOfComment = replyOfComment;
+		this.replyOfPost = replyOfPost;
+		this.location = location;
+	}
+
+	public Long getCommentID() {
+		return commentID;
+	}
+
+	public Calendar getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Calendar creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Long getCreatorID() {
+		return creatorID;
+	}
+
+	public void setCreatorID(Long creatorID) {
+		this.creatorID = creatorID;
+	}
+
+	public String getBrowserUsed() {
+		return browserUsed;
+	}
+
+	public void setBrowserUsed(String browserUsed) {
+		this.browserUsed = browserUsed;
+	}
+
+	public String getLocationIP() {
+		return locationIP;
+	}
+
+	public void setLocationIP(String locationIP) {
+		this.locationIP = locationIP;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public Long getReplyOfComment() {
+		return replyOfComment;
+	}
+
+	public void setReplyOfComment(Long replyOfComment) {
+		this.replyOfComment = replyOfComment;
+	}
+
+	public Long getReplyOfPost() {
+		return replyOfPost;
+	}
+
+	public void setReplyOfPost(Long replyOfPost) {
+		this.replyOfPost = replyOfPost;
+	}
+
+	public int getLocation() {
+		return location;
+	}
+
+	public void setLocation(int location) {
+		this.location = location;
+	}
+	
 }
--- Aufgabe 3 Maven/src/main/java/model/Post.java
+++ Aufgabe 3 Maven/src/main/java/model/Post.java
@@ -1,5 +1,145 @@
 package model;
 
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 public class Post implements Message {
 
+	@Id
+	@Column(name = "postID")
+	private Long postID;
+	
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar creationDate;
+	
+	@JoinColumn(name = "person")
+	private Long creatorID;
+	
+	@Column(length = 32)
+	private String languageName;
+	
+	@Column(length = 128)
+	private String imageFile;
+	
+	@Column(length = 64)
+	private String browserUsed;
+	
+	@Column(nullable = false)
+	private String locationIP;
+	
+	@Column(columnDefinition = "context")
+	private String context;
+	
+	private int length;
+	
+	@Column(nullable = false)
+	@JoinColumn(name = "country")
+	private int location;
+	
+	//TODO
+	/*@Column(nullable = false)
+	@JoinColumn
+	private Long containerForum;*/
+	
+	public Post() {
+	}
+	
+	public Post(Long postID, Calendar creationDate, Long creatorID, String languageName, String imageFile, String browserUsed, String locationIP, String context, int length, int location) {
+		this.postID = postID;
+		this.creationDate = creationDate;
+		this.creatorID = creatorID;
+		this.languageName = languageName;
+		this.imageFile = imageFile;
+		this.browserUsed = browserUsed;
+		this.locationIP = locationIP;
+		this.context = context;
+		this.length = length;
+		this.location = location;
+		//this.containerForum = containerForum;
+	}
+
+	public Long getPostID() {
+		return postID;
+	}
+
+	public Calendar getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Calendar creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Long getCreatorID() {
+		return creatorID;
+	}
+
+	public void setCreatorID(Long creatorID) {
+		this.creatorID = creatorID;
+	}
+
+	public String getLanguageName() {
+		return languageName;
+	}
+
+	public void setLanguageName(String languageName) {
+		this.languageName = languageName;
+	}
+
+	public String getImageFile() {
+		return imageFile;
+	}
+
+	public void setImageFile(String imageFile) {
+		this.imageFile = imageFile;
+	}
+
+	public String getBrowserUsed() {
+		return browserUsed;
+	}
+
+	public void setBrowserUsed(String browserUsed) {
+		this.browserUsed = browserUsed;
+	}
+
+	public String getLocationIP() {
+		return locationIP;
+	}
+
+	public void setLocationIP(String locationIP) {
+		this.locationIP = locationIP;
+	}
+
+	public String getContext() {
+		return context;
+	}
+
+	public void setContext(String context) {
+		this.context = context;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getLocation() {
+		return location;
+	}
+
+	public void setLocation(int location) {
+		this.location = location;
+	}
+	
+	//TODO Getter/Setter für ContainerForum
+	
 }
